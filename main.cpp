7c72a01e75045d5773c9c22a25ee56c0835b0da4
Enrico <e.sanguin.92@gmail.com>
2018-06-30 18:35:34 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "View/mainwindow.h"
 #include "Controller/datamanager.h"
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    const Punto* ca = new Punto(10,10);
-    ca->getX();
     QApplication a(argc, argv);
     MainWindow* view = new MainWindow();
     DataManager dm(view);
