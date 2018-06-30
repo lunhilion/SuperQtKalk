1b92e6a80571d3f06cd90024244026feaf01c8e5
Enrico <e.sanguin.92@gmail.com>
2018-06-30 17:09:15 +0200

    fix

--- Model/colore.h
+++ Model/colore.h
@@ -9,7 +9,7 @@ public:
     virtual Colore* operator+(const Colore&) const=0;
     virtual Colore* operator-(const Colore&) const=0;
     virtual QString getHex() const=0;
-    virtual Colore& media(const Colore&) const=0;
+    virtual Colore* media(const Colore&) const=0;
     virtual Colore* clone() const=0;
     virtual ~Colore();
     virtual unsigned int getMaxValues() const =0;
