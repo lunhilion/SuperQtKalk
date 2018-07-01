0e4e8d105f10a46272e5b6a608219517c45fd350
Tobia <tobia.apolloni@gmail.com>
2018-07-01 14:19:48 +0200

    cose

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@
 
 int main(int argc, char *argv[])
 {
-//    Colore* a = new Cmyk("#90AA14");
-//    Colore* b = new Rgb("#516264");
-//    Colore* c = a->media(*b);
-//    std::cout << c->getHex().toStdString();
     QApplication a(argc, argv);
     MainWindow* view = new MainWindow();
     DataManager dm(view);
