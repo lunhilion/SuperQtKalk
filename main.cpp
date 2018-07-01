4663bfa8b15128f982fff2685dc68d909c593786
Enrico <e.sanguin.92@gmail.com>
2018-07-01 11:15:19 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 
 int main(int argc, char *argv[])
 {
+//    Colore* a = new Cmyk("#90AA14");
+//    Colore* b = new Rgb("#516264");
+//    Colore* c = a->media(*b);
+//    std::cout << c->getHex().toStdString();
     QApplication a(argc, argv);
     MainWindow* view = new MainWindow();
     DataManager dm(view);
